532da358cb610084f804835fcaa4b8a4ac2c59c6
jayashreeH <jayashreeharlapur024@gmail.com>
2017-02-06 12:39:38 +0530

    Add files via upload

---
+++ src/app/control-buttons/control-buttons.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {appFetchDataService} from '../Services/appFetch-data.service';
+
+@Component({
+  selector: 'app-control-buttons',
+  templateUrl: './control-buttons.component.html'
+})
+export class ControlButtonsComponent implements OnInit {
+
+  constructor(appfetchDataSvc : appFetchDataService) {
+    appfetchDataSvc.refreshList$.subscribe(result => this.showElements(result));
+   }
+
+showListFlag = false;
+showAddEditFlag = false;
+
+  ngOnInit() {
+  }
+
+  showList(){
+   this.showListFlag = true;
+  }
+
+  showAddEditDialog(){
+     this.showAddEditFlag = true;
+     this.showListFlag = false;
+  }
+
+ showElements(result : boolean){
+     this.showAddEditFlag = false;
+     this.showListFlag = true;
+  }
+}

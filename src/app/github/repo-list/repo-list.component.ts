d9b1ef131b5ace571f4e0cacd9774967de97a7b9
jayashreeH <jayashreeharlapur024@gmail.com>
2017-02-06 13:43:59 +0530

    Update repo-list.component.ts

--- src/app/github/repo-list/repo-list.component.ts
+++ src/app/github/repo-list/repo-list.component.ts
@@ -5,7 +5,6 @@ import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'repo-list',
-  styleUrls: ['./repo-list.component.css'],
   templateUrl: './repo-list.component.html',
 })
 export class RepoListComponent implements OnInit {
